10724f4f016aac9b631509c07a33a7c58fe4632a
danielskatz <d.katz@ieee.org>
2020-07-09 09:28:17 -0500

    updates

--- bin/local_build_mosaic.py
+++ bin/local_build_mosaic.py
@@ -6,14 +6,17 @@ except ImportError:
     print("error:", sys.argv[0], "requires Pillow - install it via 'pip install Pillow'")
     sys.exit(2)
 
+edge_size = int(sys.argv[1])
+outputImage_name = sys.argv[2]
+
 outputImage = Image.new('RGB', [400, 400])
 index = 0
-for i in range(2):
-    for j in range(2):
-        inputImage = Image.open("{:04d}_sharp.jpg".format(index+1))
+for i in range(edge_size):
+    for j in range(edge_size):
+        inputImage = Image.open(sys.argv[index+3])
         box = ((i)*200, (j)*200, (i+1)*200, (j+1)*200)
         region = inputImage.crop(box)
         outputImage.paste(region, box)
         index=index+1
 
-outputImage.save("mosaic.jpg", 'JPEG')
+outputImage.save(outputImage_name, 'JPEG')
