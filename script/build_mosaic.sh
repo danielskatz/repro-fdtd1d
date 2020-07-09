8bcf7d80dd0f848de831584634c7b89f2544f682
danielskatz <d.katz@ieee.org>
2020-07-09 09:31:10 -0500

    simplify shell script

--- script/build_mosaic.sh
+++ script/build_mosaic.sh
@@ -1,15 +1,11 @@
 mkdir raw
 mkdir proc
-cd raw
-wget https://raw.githubusercontent.com/danielskatz/parsl-example/master/data/0001.jpg
-wget https://raw.githubusercontent.com/danielskatz/parsl-example/master/data/0002.jpg
-wget https://raw.githubusercontent.com/danielskatz/parsl-example/master/data/0003.jpg
-wget https://raw.githubusercontent.com/danielskatz/parsl-example/master/data/0004.jpg
-cd ..
-python bin/sharpen_image.py raw/0001.jpg proc/0001_sharp.jpg
-python bin/sharpen_image.py raw/0002.jpg proc/0002_sharp.jpg
-python bin/sharpen_image.py raw/0003.jpg proc/0003_sharp.jpg
-python bin/sharpen_image.py raw/0004.jpg proc/0004_sharp.jpg
-cd proc
-python ../bin/local_build_mosaic.py 2 mosaic.jpg
-cd ..
+wget https://raw.githubusercontent.com/danielskatz/parsl-example/master/data/0001.jpg -O raw/0001.jpg
+wget https://raw.githubusercontent.com/danielskatz/parsl-example/master/data/0002.jpg -O raw/0002.jpg
+wget https://raw.githubusercontent.com/danielskatz/parsl-example/master/data/0003.jpg -O raw/0003.jpg
+wget https://raw.githubusercontent.com/danielskatz/parsl-example/master/data/0004.jpg -O raw/0004.jpg
+python3 bin/sharpen_image.py raw/0001.jpg proc/0001_sharp.jpg
+python3 bin/sharpen_image.py raw/0002.jpg proc/0002_sharp.jpg
+python3 bin/sharpen_image.py raw/0003.jpg proc/0003_sharp.jpg
+python3 bin/sharpen_image.py raw/0004.jpg proc/0004_sharp.jpg
+python3 bin/local_build_mosaic.py 2 proc/mosaic.jpg proc/0001_sharp.jpg proc/0002_sharp.jpg proc/0003_sharp.jpg proc/0004_sharp.jpg
